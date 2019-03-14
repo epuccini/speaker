4fd80eb73909ab343e0266bf2453604d81a14385
Edward Puccini <edward@Edwards-MacBook-Air.local>
2019-03-14 21:18:32 +0100

    Example tuned for other platforms

--- speaker/src/example.lisp
+++ speaker/src/example.lisp
@@ -80,48 +80,47 @@ with a response."
 	store))
 
 (defun listener-setup (listener)
-  (let ((store (load-data "verbs.txt")))
-	(mapcar #'(lambda (word)
-				(add-command *listener* word)) store)
-	(add-commands listener "test" "speak" "exit" "you")
-	(speaker:start-listening listener)))
+  (add-commands listener "test" "speak" "exit" "you")
+  (speaker:start-listening listener))
 
 ;;
 ;; Main
 ;;
 (defun main ()
   "Main test program."
-(trivial-main-thread:with-body-in-main-thread () ;; main loop
-  (terpri)
-  (princ "Start listening")
-  (init-speaker)
-  (set-voice 7)
-  (speak "Test")
-
-  ;; now with object
-  (setf *speaker* (make-speaker))
-  (setf *listener* (make-listener))
-  ;; setup callbacks
-  (register-did-recognize-command-callback
-   *listener* (cffi:callback drc-callback))
-  (register-will-speak-word-callback
-   *speaker* (cffi:callback wsw-callback))
-
-  ;; setup voice, speak and listen
-  (set-voice-with *speaker* 7)
-  (listener-setup *listener*)
-  (speak-with *speaker* "Entering mainloop")
-
-  ;; setup loop
-  (setf *stop-flag* nil)
-  (loop while (not *stop-flag*) do 
-       (mainloop-speaker *speaker*)
-       (mainloop-listener *listener*))
-  ;; exit
-  (setf *stop-flag* nil)
-  (stop-listening *listener*)
-  (speak-with *speaker* "exit")
-  (cleanup-with *speaker*)))
+  (trivial-main-thread:with-body-in-main-thread () ;; main loop
+	(terpri)
+	(princ "Start speaker")
+	(init-speaker)
+	(set-voice 7)
+	(speak "Speech recognition is only supported on Mac OSX platforms")
+	
+	;; now with object
+#+darwin
+	(progn
+	  (setf *speaker* (make-speaker))
+	  (setf *listener* (make-listener))
+	  ;; setup callbacks
+	  (register-did-recognize-command-callback
+	   *listener* (cffi:callback drc-callback))
+	  (register-will-speak-word-callback
+	   *speaker* (cffi:callback wsw-callback))
+	  
+	  ;; setup voice, speak and listen
+	  (set-voice-with *speaker* 7)
+	  (listener-setup *listener*)
+	  
+	  ;; setup loop
+	  (setf *stop-flag* nil)
+	  (loop while (not *stop-flag*) do 
+		   (mainloop-speaker *speaker*)
+		   (mainloop-listener *listener*))
+	  ;; exit
+	  (setf *stop-flag* nil)
+	  (stop-listening *listener*)
+	  (speak-with *speaker* "exit")
+	  (cleanup-with *speaker*))
+  ))
 
 
 
